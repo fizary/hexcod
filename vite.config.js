d85c493267c37fa65638d8cd330aa5e3fc9616e9
Filip Kowalewski <filip.kowalewski598@gmail.com>
2025-03-11 12:36:04 +0200

    init

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+    plugins: [dts({ rollupTypes: true })],
+    build: {
+        lib: {
+            entry: resolve(import.meta.dirname, "src/index.ts"),
+            formats: ["es"],
+        },
+        outDir: "./lib",
+    },
+});
